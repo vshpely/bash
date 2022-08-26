07d6b2df3eeb5f6762acd5d041ca1e0d6f27dfc8
Vitaliy Shpelyk <vshpelyk@gmail.com>
2022-08-26 14:57:50 -0400

    Add pipeline file

---
+++ papelines.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Tasks:
+# write a one line command using || and && operators that does the following:
+# creates a folder named Name_Surname 
+# if folder creation was successful, creates a file inside, called my_file
+# if file creation was successfull, writes "Hello" into the file
+# if previous operation was successful, lists the contents of the file
+# if any of the operations fail, print "Something went wrong"
+# run the command two times
+# write a command that outputs only users that have /usr/bin/false shell from /etc/passwd file and changes the shell to /bin/bash
+
+#mkdir Igor_Obushko && touch ~/Igor_Obushko/my_file && echo "Hello" > ~/Igor_Obushko/my_file && cat ~/Igor_Obushko/my_file || echo "Something went wrong"
+
+
